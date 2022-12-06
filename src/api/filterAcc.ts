f8b0d8ff612f9ac2967bfa058461abbb72e531b7
Whisker17 <yukaijun123456@163.com>
2022-12-07 01:31:11 +0800

    update

--- src/api/filterAcc.ts
+++ src/api/filterAcc.ts
@@ -2,13 +2,13 @@ import { gql, GraphQLClient } from "graphql-request";
 import { ZEITGEIST_GQL_URL } from "../const";
 
 const filterAccountsQuery = gql`
-  query filterAccounts {
+  query filterAccounts($startTime: DateTime, $endTime: DateTime) {
     historicalAssets(
       where: {
         event_contains: "Swap"
         accountId_isNull: false
-        timestamp_gt: "2022-10-01T00:00:00Z"
-        timestamp_lt: "2022-11-25T00:00:00Z"
+        timestamp_gt: $startTime
+        timestamp_lt: $endTime
       }
       orderBy: accountId_ASC
     ) {
@@ -18,13 +18,24 @@ const filterAccountsQuery = gql`
   }
 `;
 
-export const getFilterAccounts = async (client: GraphQLClient) => {
+const setAmount = 10;
+const startTime = "2022-10-01T00:00:00Z";
+const endTime = "2022-11-25T00:00:00Z";
+
+export const getFilterAccounts = async (
+  client: GraphQLClient,
+  startTime: String,
+  endTime: String
+) => {
   const response = await client.request<{
     historicalAssets: {
-      accountId: string[];
-      ztgTraded: Number[];
+      accountId: string;
+      ztgTraded: Number;
     }[];
-  }>(filterAccountsQuery);
+  }>(filterAccountsQuery, {
+    startTime: startTime,
+    endTime: endTime,
+  });
 
   return response.historicalAssets;
 };
@@ -34,9 +45,31 @@ async function main() {
    * Fetching asset indexes works with both rpc and indexer mode.
    */
   const endPoint = new GraphQLClient(ZEITGEIST_GQL_URL);
-  const res = await getFilterAccounts(endPoint);
+  const res = await getFilterAccounts(endPoint, startTime, endTime);
 
-  console.log(res);
+  let accs = new Array<String>();
+  let tempAccZTGPair = new Map();
+  res.forEach((hisAssets) => {
+    if (
+      tempAccZTGPair.has(hisAssets.accountId) &&
+      tempAccZTGPair.get(hisAssets.accountId) > setAmount * 10 ** 10
+    ) {
+      if (!accs.includes(hisAssets.accountId)) {
+        accs.push(hisAssets.accountId);
+      }
+      return;
+    }
+    if (!tempAccZTGPair.has(hisAssets.accountId)) {
+      tempAccZTGPair.set(hisAssets.accountId, hisAssets.ztgTraded);
+    } else {
+      tempAccZTGPair.set(
+        hisAssets.accountId,
+        tempAccZTGPair.get(hisAssets.accountId) + hisAssets.ztgTraded
+      );
+    }
+  });
+
+  console.log(accs);
 }
 
 main().catch((error) => {
