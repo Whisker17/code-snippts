ad52bcf66e5ef478f18cc73a87223346e054b2ad
Whisker17 <yukaijun123456@163.com>
2022-12-14 19:26:49 +0800

    update

--- src/test.ts
+++ src/test.ts
@@ -1,53 +1,25 @@
-const infos = [
-  { tags: ["Politics"] },
-  { tags: ["Politics"] },
-  { tags: ["E-sports", "Space", "Technology"] },
-  { tags: ["North America", "Politics"] },
-  { tags: [] },
-  { tags: ["North America", "Politics"] },
-  { tags: ["North America", "Politics"] },
-  { tags: ["North America", "Politics"] },
-  { tags: ["Democracy", "Politics"] },
-  { tags: ["Elections", "Politics"] },
-  { tags: ["Crypto"] },
-  { tags: ["Elections", "Governance", "Politics"] },
-  { tags: ["Crypto"] },
-  { tags: ["Crypto"] },
-  { tags: ["Democracy", "Politics"] },
-  {
-    tags: [
-      "COVID-19",
-      "China",
-      "Crypto",
-      "Democracy",
-      "North America",
-      "Politics",
-    ],
-  },
-  { tags: [] },
-  { tags: ["COVID-19"] },
-  { tags: ["Space", "Technology"] },
-  { tags: ["Politics"] },
-];
+export interface APPCounts {
+  message: { count: number; date: string }[];
+}
 
-const tests = [{ tags: ["1"] }, { tags: ["1", "2"] }, { tags: [] }];
+export interface APPCountsChart {
+  message: { start: string; end: string; count: number }[];
+}
+
+const fetchAPPCounts = (name: string): Promise<APPCounts> => {
+  return fetch(`https://pro-api.zeitgeist.pm/api/${name}`)
+    .then((response: Response) => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+      return 0;
+    });
+};
 
 async function main() {
-  let i = 0;
-  const res = infos.reduce((arr, curr) => {
-    if (curr.tags.length === 0) {
-      i++;
-    }
-    curr.tags.forEach((index) => {
-      if (!arr.has(index)) {
-        arr.set(index, 1);
-      } else {
-        arr.set(index, Number(arr.get(index)) + 1);
-      }
-    });
-    return arr;
-  }, new Map());
-  res.set("Others", i);
+  const res = fetchAPPCounts("trades");
   console.log(res);
 }
 
