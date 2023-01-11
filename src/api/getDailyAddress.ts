a2be4b035bef34e22db5f4517cd5c99b310b741b
Whisker17 <yukaijun123456@163.com>
2023-01-11 23:57:39 +0800

    update

--- src/api/getDailyAddress.ts
+++ src/api/getDailyAddress.ts
@@ -1,29 +1,102 @@
 import fetch from "node-fetch";
+import FormData from "form-data";
 
-const fetchTransactionsCount = (): Promise<number> => {
-  return fetch(`https://zeitgeist.api.subscan.io/api/scan/metadata`, {
-    headers: {
-      "x-api-keys": String("597819365d984673938c35c37f35b9c2"),
-    },
-  })
-    .then((response: Response) => {
+export interface user {
+  active: number;
+  users: number;
+  day: string;
+  total?: number;
+}
+
+const fetchTransactionsCount = async (date: String): Promise<user[]> => {
+  const user = new Array<user>();
+  const d = new FormData();
+  d.append("start", "2022-06-15");
+  d.append("end", `${date}`);
+  d.append("format", "day");
+  d.append("category", "ActiveAccount");
+  const active = await fetch(
+    `https://zeitgeist.api.subscan.io/api/scan/daily`,
+    {
+      method: "POST",
+      headers: {
+        "x-api-keys": String("597819365d984673938c35c37f35b9c2"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: d,
+    }
+  )
+    .then((response) => {
       if (!response.ok) throw new Error(response.statusText);
       return response.json();
     })
-    .then((response) => {
-      return response;
+    .then((response: any) => {
+      response.data.list.forEach((value: any) => {
+        user.push({
+          day: value.time_utc.slice(0, 10),
+          active: value.total,
+          users: 0,
+          total: 0,
+        });
+      });
+      return user;
     })
     .catch((error) => {
       console.log(error);
-      return 0;
+      return new Array<user>();
     });
+
+  const dd = new FormData();
+  dd.append("start", "2022-06-15");
+  dd.append("end", `${date}`);
+  dd.append("format", "day");
+  dd.append("category", "NewAccount");
+  const newAccount = await fetch(
+    `https://zeitgeist.api.subscan.io/api/scan/daily`,
+    {
+      method: "POST",
+      headers: {
+        "x-api-keys": String("597819365d984673938c35c37f35b9c2"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: dd,
+    }
+  )
+    .then((response) => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
+    .then((response: any) => {
+      response.data.list.forEach((value: any, index: number) => {
+        active[index].users = value.total;
+        if (index == 0) {
+          active[index].total = value.total;
+        } else {
+          active[index].total = active[index - 1].total + value.total;
+        }
+      });
+      return active;
+    })
+    .catch((error) => {
+      console.log(error);
+      return new Array<user>();
+    });
+  return newAccount;
 };
 
 async function main() {
   /**
    * Fetching asset indexes works with both rpc and indexer mode.
    */
-  const res = await fetchTransactionsCount();
+
+  const yesterday = new Date(
+    Date.now() -
+      1 * 864e5 -
+      new Date(Date.now() - 1 * 864e5).getTimezoneOffset() * 6e4
+  )
+    .toISOString()
+    .split("T")[0];
+  const res = await fetchTransactionsCount(yesterday);
 
   console.log(res);
 }
--- src/test.ts
+++ src/test.ts
@@ -1,26 +1,62 @@
-const data = [
-  { tag: "Politics", count: 12 },
-  { tag: "E-sports", count: 2 },
-  { tag: "Space", count: 1 },
-  { tag: "Technology", count: 1 },
-  { tag: "North America", count: 5 },
-  { tag: "Democracy", count: 3 },
-  { tag: "Elections", count: 2 },
-  { tag: "Crypto", count: 4 },
-  { tag: "Governance", count: 1 },
-  { tag: "COVID-19", count: 1 },
-  { tag: "China", count: 1 },
-  { tag: "Others", count: 3 },
-];
+import fetch from "node-fetch";
 
-async function main() {
-  data.splice(
-    data.findIndex((item) => item.tag === "Others"),
-    1
-  );
-  console.log(data);
+export interface TokenInfos {
+  price: string;
+  price_change: string;
+  total_issuance: number;
+  free_balance: number;
+  available_balance: number;
+  locked_balance: number;
+  reserved_balance: number;
+  bonded_locked_balance: number;
+  democracy_locked_balance: number;
+  vesting_balance: number;
 }
 
-main()
-  .catch(console.error)
-  .finally(() => process.exit());
+const fetchTokenInfos = (): Promise<TokenInfos> => {
+  const token: TokenInfos = {} as TokenInfos;
+  return fetch(`https://zeitgeist.api.subscan.io/api/scan/token`, {
+    headers: {
+      "x-api-keys": String("597819365d984673938c35c37f35b9c2"),
+    },
+  })
+    .then((response: any) => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
+    .then((response) => {
+      const ztg = response.data.detail.ZTG;
+      token.price = ztg.price;
+      token.price_change = ztg.price_change;
+      token.total_issuance = Number(ztg.total_issuance) / 10 ** 10;
+      token.free_balance = Number(ztg.free_balance) / 10 ** 10;
+      token.available_balance = Number(ztg.available_balance) / 10 ** 10;
+      token.locked_balance = Number(ztg.locked_balance) / 10 ** 10;
+      token.reserved_balance = Number(ztg.reserved_balance) / 10 ** 10;
+      token.bonded_locked_balance =
+        Number(ztg.bonded_locked_balance) / 10 ** 10;
+      token.democracy_locked_balance =
+        Number(ztg.democracy_locked_balance) / 10 ** 10;
+      token.vesting_balance = Number(ztg.vesting_balance) / 10 ** 10;
+      return response.data.count_transfer;
+    })
+    .catch((error) => {
+      console.log(error);
+      return 0;
+    });
+};
+
+async function main() {
+  /**
+   * Fetching asset indexes works with both rpc and indexer mode.
+   */
+
+  const res = await fetchTokenInfos();
+
+  console.log(res);
+}
+
+main().catch((error) => {
+  console.log("ERROR");
+  console.log(error);
+});
